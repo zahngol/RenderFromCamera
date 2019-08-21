f1ad3a83d22e207fa8b36f6fc53465f3c903ff72
Zach <zahngol@gmail.com>
2019-08-21 15:48:18 -0400

    Cleaned up implementation
    - Added comments
    - Abstracted some of the function
    - Set up the RenderFromCamera to return a Texture2D rather than writing out the file itself

--- Assets/Scripts/RenderAllCameras.cs
+++ Assets/Scripts/RenderAllCameras.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// Temporary implementation of the RenderFromCamera class
 public class RenderAllCameras : MonoBehaviour
 {
+    // Array that will hold all RenderFromCamera components in the scene
     private RenderFromCamera[] cameraRenderComponents;
 
     private void Awake()
     {
+        // Cache all of the RenderFromCamera components 
         cameraRenderComponents = FindObjectsOfType<RenderFromCamera>();
     }
     
-    // Update is called once per frame
     private void Update()
     {
+        // When the space bar is pressed, create a render for each of the cameras with the RenderFromCamera component
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            foreach (var renderComponent in cameraRenderComponents)
+            foreach (RenderFromCamera renderComponent in cameraRenderComponents)
             {
-                renderComponent.CreateRender();
+                // Get a Texture2D from the RenderFromCamera component
+                Texture2D temp = renderComponent.CreateRender();
+
+                WritePNGToFile(
+                    temp,
+                    $"{Application.dataPath}/Snapshots/{renderComponent.gameObject.name} {System.DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png"
+                    );
             }
         }
+    }
+
+    private void WritePNGToFile(Texture2D texture, string path)
+    {
+        byte[] bytes = texture.EncodeToPNG();
+        System.IO.File.WriteAllBytes(path, bytes);
     }
 }
--- Assets/Scripts/RenderFromCamera.cs
+++ Assets/Scripts/RenderFromCamera.cs
@@ -1,49 +1,56 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// Component placed on a camera that can be used to create a Texture2D of the camera's view
 [RequireComponent(typeof(Camera))]
 public class RenderFromCamera : MonoBehaviour
 {
+    // The camera component for this game object
     private Camera cameraComponent;
     
+    // A texture 2D to be used as a storage location for camera renders
+    private Texture2D texture;
+
+    // These will probably be stored somewhere else in the future
     private int resWidth = 1920;
     private int resHeight = 1080;
-        
-    // Start is called before the first frame update
+    
     void Start()
     {
+        // Cache the camera component
         cameraComponent = GetComponent<Camera>();
 
+        // If there is no RenderTexture assigned to the target texture, create one
         if (cameraComponent.targetTexture == null)
         {
             cameraComponent.targetTexture = new RenderTexture(resWidth, resHeight, 24);
         }
 
+        // Disable the camera component until it is required to render an image
+        // Disabling the component rather than the object so that scripts attached to the object will still function
         cameraComponent.enabled = false;
     }
 
-    public void CreateRender()
+    public Texture2D CreateRender()
     {
+        // Enable the camera component
         cameraComponent.enabled = true;
-    }
+        
+        // Reset the texture properties to make sure it matches the desired resolution
+        texture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        
+        // Force the camera to render
+        cameraComponent.Render();
+        
+        // Update the RenderTexture with the camera's targetTexture
+        RenderTexture.active = cameraComponent.targetTexture;
+        
+        // Reaad the pixels from the render texture into the Texture2D
+        texture.ReadPixels(new Rect(0,0,resWidth, resHeight), 0,0);
+        
+        // Disable the camera component until it's needed again
+        cameraComponent.enabled = false;
 
-    private void LateUpdate()
-    {
-        if (cameraComponent.enabled)
-        {
-            Texture2D texture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            cameraComponent.Render();
-            RenderTexture.active = cameraComponent.targetTexture;
-            texture.ReadPixels(new Rect(0,0,resWidth, resHeight), 0,0);
-            byte[] bytes = texture.EncodeToPNG();
-            string filename = BuildFileName();
-            System.IO.File.WriteAllBytes(filename, bytes);
-            cameraComponent.enabled = false;
-        }
-    }
-
-    private string BuildFileName()
-    {
-        return $"{Application.dataPath}/Snapshots/{gameObject.name} {DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+        // Return the texture
+        return texture;
     }
 }
