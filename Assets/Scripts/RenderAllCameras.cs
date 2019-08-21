43af1c645314016d899d121bfa9ee29000817138
Zach <zahngol@gmail.com>
2019-08-21 14:58:15 -0400

    Added code to render out images from cameras

---
+++ Assets/Scripts/RenderAllCameras.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderAllCameras : MonoBehaviour
+{
+    private RenderFromCamera[] cameraRenderComponents;
+
+    private void Awake()
+    {
+        cameraRenderComponents = FindObjectsOfType<RenderFromCamera>();
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var renderComponent in cameraRenderComponents)
+            {
+                renderComponent.CreateRender();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RenderFromCamera.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RenderFromCamera : MonoBehaviour
+{
+    private Camera cameraComponent;
+    
+    private int resWidth = 1920;
+    private int resHeight = 1080;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraComponent = GetComponent<Camera>();
+
+        if (cameraComponent.targetTexture == null)
+        {
+            cameraComponent.targetTexture = new RenderTexture(resWidth, resHeight, 24);
+        }
+
+        cameraComponent.enabled = false;
+    }
+
+    public void CreateRender()
+    {
+        cameraComponent.enabled = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (cameraComponent.enabled)
+        {
+            Texture2D texture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            cameraComponent.Render();
+            RenderTexture.active = cameraComponent.targetTexture;
+            texture.ReadPixels(new Rect(0,0,resWidth, resHeight), 0,0);
+            byte[] bytes = texture.EncodeToPNG();
+            string filename = BuildFileName();
+            System.IO.File.WriteAllBytes(filename, bytes);
+            cameraComponent.enabled = false;
+        }
+    }
+
+    private string BuildFileName()
+    {
+        return $"{Application.dataPath}/Snapshots/{gameObject.name} {DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+    }
+}
